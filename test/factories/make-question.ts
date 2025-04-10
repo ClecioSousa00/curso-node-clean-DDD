43f35ee45d89566daa22d9ed77adcdca20b9e348
ClecioSousa000 <cleciosouza015@gmail.com>
2025-04-10 09:33:49 -0300

    Feat: aula 143

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -4,15 +4,22 @@ import {
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { faker } from '@faker-js/faker'
 
-export const makeQuestion = (override: Partial<QuestionProps> = {}) => {
-  const question = Question.create({
-    title: 'test question',
-    authorId: new UniqueEntityID(),
-    slug: Slug.create('test-question'),
-    content: ' test question',
-    ...override,
-  })
+export const makeQuestion = (
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) => {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      authorId: new UniqueEntityID(),
+      slug: Slug.create('test-question'),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
