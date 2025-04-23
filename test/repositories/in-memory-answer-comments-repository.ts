7940625b12b64cf6870a8918cb16791114457ec9
ClecioSousa000 <cleciosouza015@gmail.com>
2025-04-23 19:46:53 -0300

    Feat: aula 166

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -8,4 +8,18 @@ export class InMemoryAnswerCommentsRepository
   async create(answer: AnswerComment) {
     this.items.push(answer)
   }
+
+  async findById(id: string): Promise<AnswerComment | null> {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    return answerComment ?? null
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
+  }
 }
